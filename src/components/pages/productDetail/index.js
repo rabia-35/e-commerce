b673c34c55700488d0cba8ccba535803b0059416
rabia ölker <rabia.olker@gmail.com>
2022-05-12 19:16:04 +0300

    product detail listed

--- src/components/pages/productDetail/index.js
+++ src/components/pages/productDetail/index.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import {useSelector, useDispatch} from "react-redux";
+import {Container, Row, Col, Button} from "react-bootstrap"
+import {useParams} from  "react-router-dom";
+import {addBasket} from "../../../redux/products/productsSlice"
+
 
 function ProductDetail() {
+  const {id}=useParams()
+  const dispatch=useDispatch()
+  const items=useSelector(state=>state.product.items)
+  const basketProd=useSelector(state=>state.product.basket)
+  console.log(basketProd)
+  const detail=items.filter(item=>item.id===id)
+  
+  
   return (
-    <div>ProductDetail</div>
+    <Container>
+      {
+        detail &&(
+          <Row className="mt-5">
+          <Col xs={12} sm={5} >
+          <img  className="detail-img" alt='characterImage' src={detail[0].img} />
+          </Col>
+          <Col className='detail-desc' >
+            <h5>{detail[0].title} </h5>
+            <p>  {detail[0].description}</p>
+            <p> <strong className='text-muted'>Price :</strong> {detail[0].price} tl</p>
+            <Button className='btn-btn-success' onClick={()=>dispatch(addBasket([detail[0]]))} >Basket Add</Button>
+          </Col>
+      </Row>
+        )
+      }
+    </Container>
   )
 }
 
