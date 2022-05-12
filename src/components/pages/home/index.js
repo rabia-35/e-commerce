14dd9343a6fbe5b45b3a6511c5b4d3fe46885e23
rabia ölker <rabia.olker@gmail.com>
2022-05-12 19:13:36 +0300

    divided by product categories in home

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {useSelector, useDispatch} from "react-redux"
-import {Container, Row, Col, Card} from "react-bootstrap"
+import {Container, Row, Col, Card, Image} from "react-bootstrap"
 import {Link} from "react-router-dom"
 import { filtered } from '../../../redux/products/productsSlice'
 function Home() {
@@ -16,32 +16,26 @@ dispatch(filtered(category))
       <Row xs={1} sm={3}  >
         <Col >
           <Link to={`/category/${items[5].category}`} onClick={()=>handleClick(items[5].category)} >
-              <Card  className='category' >
-                <Card.Img variant="top" src={items[5].img} className="home-img" />
-                <Card.Body>
-                  <Card.Title>{items[5].category}s </Card.Title>
-                </Card.Body>
-              </Card>
+              <div  className='category' >
+                <Image variant="top" src={items[5].img}  />
+                  <h5>{items[5].category}s </h5>
+              </div>
           </Link>
         </Col>
         <Col >
           <Link to={`/category/${items[10].category}`} onClick={()=>handleClick(items[10].category)} >
-              <Card className='category'>
-                <Card.Img variant="top" src={items[10].img} />
-                <Card.Body>
-                  <Card.Title>{items[10].category}s</Card.Title>
-                </Card.Body>
-              </Card>
+              <div className='category'>
+                <Image variant="top" src={items[10].img} />
+                  <h5>{items[10].category}s</h5>
+              </div>
           </Link>
         </Col>
         <Col>
           <Link to={`/category/${items[16].category}`} onClick={()=>handleClick(items[16].category)} >
-              <Card className='category'>
-                <Card.Img variant="top" src={items[16].img} />
-                <Card.Body>
-                  <Card.Title>{items[16].category}s</Card.Title>
-                </Card.Body>
-              </Card>
+              <div className='category'>
+                <Image variant="top" src={items[16].img} />
+                  <h5>{items[16].category}s</h5>
+              </div>
           </Link>
         </Col>
       </Row>
