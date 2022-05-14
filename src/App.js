d8506bb386def0e1ebdcdeed85081f26b463376b
rabia ölker <rabia.olker@gmail.com>
2022-05-15 00:59:56 +0300

    Added fixed="top" info to Navbar

--- src/App.js
+++ src/App.js
@@ -7,37 +7,39 @@ import ProductDetail from "./components/pages/productDetail"
 import Basket from "./components/pages/basket"
 import {useSelector} from "react-redux"
 
+
 /** */
 function App() {
   const basket=useSelector(state=>state.product.basket)
   return (
       <div className='App-header'>
-          
           <BrowserRouter>
-          <Navbar bg="light" expand="lg" >
-            <Container>
-              
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                  <Nav.Link href="/">Home</Nav.Link>
-                  
-                  <NavDropdown title="Category" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="/category/Food">Food</NavDropdown.Item>
-                    <NavDropdown.Item href="/category/Beverage">Beverage</NavDropdown.Item>
-                    <NavDropdown.Item href="/category/Technological">Technological</NavDropdown.Item>
-                  </NavDropdown>
-                  
-                </Nav>
-                <Link to="/basket" className="ms-auto btn btn-sm btn-warning ">Basket {basket!==null && (basket.length)}</Link>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
+             <Container className='nav-css'>
+                <Navbar fixed="top"  bg="light" expand="lg" >
+                    <Container>
+                      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                      <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                          <Nav.Link href="/">Home</Nav.Link>
+                          
+                          <NavDropdown title="Category" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/category/Food">Food</NavDropdown.Item>
+                            <NavDropdown.Item href="/category/Beverage">Beverage</NavDropdown.Item>
+                            <NavDropdown.Item href="/category/Technological">Technological</NavDropdown.Item>
+                          </NavDropdown>
+                          
+                        </Nav>
+                        <Link to="/basket" className="ms-auto btn btn-sm btn-warning ">Basket {basket!==null && (basket.length)}</Link>
+                      </Navbar.Collapse>
+                    </Container>
+                  </Navbar>
+             </Container>
             <Routes>
               <Route  exact path="/" element={<Home />} />
               <Route  path="/category/:category" element={<Products />} />
               <Route path="/:category/products/:id" element={<ProductDetail />} />
-              <Route  path="/basket" element={<Basket />} />
+              <Route  exact path="/basket" element={<Basket />} />
+              
             </Routes>
           </BrowserRouter>
       </div>
